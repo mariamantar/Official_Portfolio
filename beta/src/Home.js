1384ca0a51be0b9001d969c8dfaa2b60ea4b4f33
Mariam <mariam.antar@hiruep.com.au>
2020-02-08 13:59:01 +1100

    added content

--- beta/src/Home.js
+++ beta/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import { FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -16,24 +17,91 @@ const Home = () => {
         </header>
         <div className="content">
           <p>
-            I am a software engineer from Sydney, Australia.
-            djhbfjsbjfbjdfbsfjsb
+            Software engineer and tech head from Sydney, Australia. I build and
+            repair things. 🛠
           </p>
-          <p>I work at hireup</p>
+          <p>
+            I currently work at{' '}
+            <span>
+              <a
+                href="https://www.hireup.com.au/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hireup
+              </a>
+            </span>
+            , an online platform helping people seeking support.
+          </p>
         </div>
         <div>
           <h2>1.0_2-Work.mp3</h2>
           <div className="work">
             <ul>
-              <li>Check out my project here on github</li>
-              <li>Some of my blogposts</li>
-              <li>View some of my repairs and more on youtube</li>
+              <li>
+                Check out my projects on{' '}
+                <span>
+                  <a
+                    href="https://github.com/mariamantar"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    github
+                  </a>
+                </span>
+              </li>
+              <li>
+                Read my blogposts on{' '}
+                <span>
+                  <a
+                    href="https://dev.to/mariamantar"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Dev.to
+                  </a>
+                </span>
+              </li>
+              <li>
+                Watch my videos on{' '}
+                <span>
+                  <a
+                    href="https://www.youtube.com/channel/UC4luHbFfHOuMCcQM4TKZNTw"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    youtube
+                  </a>
+                </span>
+              </li>
             </ul>
           </div>
         </div>
         <div className="contact">
           <h2>1.0_3-Contact.sd</h2>
           <p>Email: mariamantar1[at]hotmail.com</p>
+        </div>
+        <div className="social">
+          <ul>
+            <li>
+              <a
+                href="https://twitter.com/mariamantar_"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitterSquare />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/mariamantar/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
         </div>
       </main>
     </div>
